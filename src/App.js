6378057a4facec9485b9780c02efd2d0ce282411
Jack Mulligan <mulligja1@gmail.com>
2022-09-11 18:41:09 +0100

    Removed commented out section for blog posts

--- src/App.js
+++ src/App.js
@@ -58,8 +58,6 @@ const App = () => {
       <Routes>
         <Route path="/" exact element={<Home ref={titleRef} />} />
       </Routes>
-      {/* {false && <Route path="/blog" exact component={Blog} />}
-      {false && <Route path="/blog/:id" component={BlogPost} />} */}
       <Footer name={`${mainBody.firstName} ${mainBody.lastName}`}>
         {getInTouch.show && (
           <GetInTouch
